e05eec92fa9bdd782f6cd81f3265622c451b2572
Jorge Gonzalez <jag2kn@gmail.com>
2012-09-11 21:12:43 -0500

    app.yaml  * Se cambia la versión de 2.1 a 3
    blog/decorators.py
     * Se incluye y se utiliza la funcion isadmin() para no usar unicamente la
       de appengine
    
    blog/static/images/FondoPagina.png
     * Se agregan archivos
    
    blog/templates/base.html
     * Se organizan los archivos de cabecera html y de la pagina
    
    blog/templates/base.html
    blog/templates/footer.html
    blog/templates/header.html
    blog/templates/list_posts.html
    blog/templates/modulo.html
    blog/templates/patrocinios.html
    blog/templates/post.html
    blog/templates/redes_sociales/twitter.html
    blog/views.py
     * Se implementa nuevo tema Ticket #11
    
    blog/templates/edit_post.html
    blog/templates/new_post.html
     * Se ajustan estilos del editor tinyedit
    
    blog/templates/post.html
     * Se parametrizan los links de edición y borrado de acuerdo al permiso admin
    
    blog/views.py
     * Se implementan funciones get_modulos, get_talleres(), get_auditorios() para
       parametrizarlas posteriormente
     * Se usa la funcion isadmin()
     * Se implementan mas variables utiles para general_data()
     * Se parametriza la cabecera de acuerdo a la pagina actual
     * Se implementa funcion demo para los talleres y auditorios
    
    Otros
     * Se agregan imagenes y archivos necesarios para los cambios mensionados

--- blog/decorators.py
+++ blog/decorators.py
@@ -2,6 +2,16 @@ from functools import wraps
 from google.appengine.api import users
 from flask import redirect, request
 
+def isadmin():
+    if not users.get_current_user():
+        return False
+    usuario = users.get_current_user()
+    if usuario.email() == "jag2kn@gmail.com":
+        return True
+    if users.is_current_user_admin():
+        return True
+    return False
+
 def login_required(func):
     @wraps(func)
     def decorated_view(*args, **kwargs):
@@ -15,6 +25,8 @@ def admin_required(func):
     def decorated_view(*args, **kwargs):
         if not users.get_current_user():
             return redirect(users.create_login_url(request.url))
+        if (isadmin()):
+            return func(*args, **kwargs)
         if not users.is_current_user_admin():
             return redirect('/')
         return func(*args, **kwargs)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from blog import app
 from models import Post
-from decorators import login_required, admin_required
+from decorators import login_required, admin_required, isadmin
 
 from flask import render_template, flash, url_for, redirect
 from flaskext import wtf
@@ -24,6 +24,85 @@ providers = {
     # add more here
 }
 
+def get_modulos():
+    return {
+            1:{
+                "titulo":u"Internet, sociedad de información y mecanismos de participación",
+                "autor":u"Michael Bowens",
+                "institucion":u"P2P Fundation",
+                "fecha":"Sep 27"},
+            2:{
+                "titulo":u"Libertad de expresión",
+                "autor":u"",
+                "institucion":u"",
+                "fecha":"Oct 11"},
+            3:{
+                "titulo":"Privacidad",
+                "autor":u"",
+                "institucion":u"",
+                "fecha":"Oct 25"},
+            4:{
+                "titulo":"Derechos de autor y excepciones y limitaciones",
+                "autor":u"",
+                "institucion":u"",
+                "fecha":"Nov 8"},
+            5:{
+                "titulo":"Gobernanza de Internet",
+                "autor":u"",
+                "institucion":u"",
+                "fecha":"Nov 22"},
+            6:{
+                "titulo":"Neutralidad de la red",
+                "autor":u"",
+                "institucion":u"",
+                "fecha":"Dic 6"},
+            7:{
+                "titulo":"Derecho a Acceso",
+                "autor":u"",
+                "institucion":u"",
+                "fecha":"Dic 13"},
+            8:{
+                "titulo":"Procedimientos para bloqueo de Contenidos en Internet",
+                "autor":u"",
+                "institucion":u"",
+                "fecha":"Ene 17"}
+        }
+    
+    
+def get_talleres():
+    return {
+            1:{
+                "titulo":u"Redes sociales para el activismo Talleristas",
+                "autor":u"",
+                "institucion":u"",#Hacktivistas
+                "fecha":"Sep 27"},
+            2:{
+                "titulo":u"Crowdfounding y mecanismos de financiación Tallersitas",
+                "autor":u"",
+                "institucion":u"",#Goteo Funding
+                "fecha":"Sep 27"},
+            3:{
+                "titulo":u"Seguridad y privacidad Tallerista",
+                "autor":u"",#Daniel Torres
+                "institucion":u"RedPaTodos",
+                "fecha":"Sep 27"},
+            4:{
+                "titulo":u"Licenciamiento abierto Tallerista",
+                "autor":u"Pilar Sáenz y Maritza Sánchez",
+                "institucion":u"Fundación Karisma",
+                "fecha":"Sep 27"}
+        }
+        
+def get_auditorios():
+    return {
+            1:{
+                "titulo":u"Hackbo",
+                "autor":u"",
+                "institucion":u"",#Hackbo
+                "fecha":""}
+        }
+        
+    
 
 class PostForm(wtf.Form):
     title = wtf.TextField('Title', validators=[validators.Required()])
@@ -43,28 +122,38 @@ def general_data():
     datos={}
     
     datos["servidor"]=request.url.replace("/", "").split(":")[1]
+    datos["url"]=request.url
+    datos["descripcion"]="Internet Activa"
+    datos["titulo"]="Internet Activa"
+    
+    datos['imagenCabecera']='images/ImagenCabecera.png'
+    logging.info(request.__dict__)
+    logging.info("************* "+request.environ['PATH_INFO'])
+    usuario = users.get_current_user()
+    logging.info("--------------- "+usuario.email())
 
     
     usuario = users.get_current_user()
     datos["usuario"] = usuario
+
+    datos["admin"] = isadmin()
     
     linksPrincipales = []
     linksPrincipales.append(('/', "Home"))
-    linksPrincipales.append(('/Modulos', "Modulos"))
+    linksPrincipales.append(('/Metodologia', u'Metodología'))
     linksPrincipales.append(('/Foros', "Foros"))
     linksPrincipales.append(('/Nosotros', "Acerca de"))
     datos["menu1"]=linksPrincipales
 
-    modulos=[]
-    for i in range(1,8+1):
-        modulos.append(('/modulo/'+str(i), "Modulo "+str(i)))
-    datos["menu2"] = modulos
+    datos["modulos"] = get_modulos()
+    datos["talleres"] = get_talleres()
+    datos["auditorios"] = get_auditorios()
     
     linksLaterales = []
     linksLaterales.append(('/Calendario', "Calendario"))
-    linksLaterales.append(('/Modulos', "Contacto"))
+    linksLaterales.append(('/Contacto', "Contacto"))
     if usuario:
-        if users.is_current_user_admin():
+        if isadmin():
             linksLaterales.append((url_for('list_posts'), "Todos las entradas"))
             linksLaterales.append((url_for('new_post'), "Nuevo articulo"))
         linksLaterales.append((users.create_logout_url("/"), "Salir"))
@@ -75,13 +164,20 @@ def general_data():
     return datos
 
 
+
+    
+
+
 @app.route('/')
 def redirect_to_home():
     return redirect('/Inicio')
 
 @app.route('/Inicio')
 def inicio():
-    titulos=[""]
+    titulos=[
+        u"Un curso en línea para que activemos la Red"]
+        #u"Internet Activa",
+        #u"Currículo del primer curso"]
     posts=[]
     
     for t in titulos:
@@ -93,7 +189,8 @@ def inicio():
     q = db.Query(Post)
     ps = q.filter('noticia =', True).order('-when')
     for p in ps:
-        posts.append(p)
+        if not p.title in titulos:
+            posts.append(p)
         
     return render_template('list_posts.html', d=general_data(),posts=posts)
 
@@ -159,17 +256,11 @@ def modulo(id):
     descripcion = "ESTAMOS TRABAJANDO EN ESTOS CONTENIDOS ESTAREMOS PUBLICANDO LA INFORMACION MUY PRONTO!"
     dias = "12/23"
     mes = "Sep"
+    modulos = get_modulos()
     if posts.count() == 0:
-        titulos={
-            2:"Libertad de expresi&oacute;n",
-            3:"Privacidad",
-            4:"Derechos de autor y excepciones y limitaciones",
-            5:"Gobernanza de Internet",
-            6:"Neutralidad de la red",
-            7:"Derecho a Acceso",
-            8:"Procedimientos para bloqueo de Contenidos en Internet"
-            }
-        titulo = titulos[id]
+        titulo = modulos[id]["titulo"]
+        mes,dias = modulos[id]["fecha"].split(" ")
+        
     else:
         post=posts[0]
         datos=post.content.split("|")
@@ -181,6 +272,69 @@ def modulo(id):
         if len(datos)>3:
             descripcion = datos[3]
     return render_template('modulo.html', d=general_data(),numero=id, titulo=titulo, descripcion=descripcion, mes=mes, dias=dias)
+
+@app.route('/taller/<int:id>/', methods=['GET', 'POST'])
+def taller(id):
+
+    '''
+    ToDo: Se utilizo la entidad Post para guardar la información de los modulos
+    esto es temporal y solo es para visualizar el template de los modulos con videos
+    Implementar entidad Modulo
+    '''
+
+    q = db.Query(Post)
+    posts = q.filter('title =', "taller_"+str(id))
+    descripcion = "ESTAMOS TRABAJANDO EN ESTOS CONTENIDOS ESTAREMOS PUBLICANDO LA INFORMACION MUY PRONTO!"
+    dias = "12/23"
+    mes = "Sep"
+    modulos = get_modulos()
+    if posts.count() == 0:
+        titulo = modulos[id]["titulo"]
+        mes,dias = modulos[id]["fecha"].split(" ")
+        
+    else:
+        post=posts[0]
+        datos=post.content.split("|")
+        titulo = datos[0].replace("<p>", "").replace("</p>", "")
+        if len(datos)>1:
+            mes = datos[1]
+        if len(datos)>2:
+            dias = datos[2]
+        if len(datos)>3:
+            descripcion = datos[3]
+    return render_template('taller.html', d=general_data(),numero=id, titulo=titulo, descripcion=descripcion, mes=mes, dias=dias)
+
+@app.route('/auditorio/<string:id>/', methods=['GET', 'POST'])
+def auditorio(id):
+
+    '''
+    ToDo: Se utilizo la entidad Post para guardar la información de los modulos
+    esto es temporal y solo es para visualizar el template de los modulos con videos
+    Implementar entidad Modulo
+    '''
+
+    q = db.Query(Post)
+    posts = q.filter('title =', "auditorio_"+id)
+    descripcion = "ESTAMOS TRABAJANDO EN ESTOS CONTENIDOS ESTAREMOS PUBLICANDO LA INFORMACION MUY PRONTO!"
+    dias = "12/23"
+    mes = "Sep"
+    modulos = get_modulos()
+    if posts.count() == 0:
+        titulo = modulos[id]["titulo"]
+        mes,dias = modulos[id]["fecha"].split(" ")
+        
+    else:
+        post=posts[0]
+        datos=post.content.split("|")
+        titulo = datos[0].replace("<p>", "").replace("</p>", "")
+        if len(datos)>1:
+            mes = datos[1]
+        if len(datos)>2:
+            dias = datos[2]
+        if len(datos)>3:
+            descripcion = datos[3]
+    return render_template('taller.html', d=general_data(),numero=id, titulo=titulo, descripcion=descripcion, mes=mes, dias=dias)
+
 
 
 
@@ -228,12 +382,14 @@ def save_post():
             return render_template('error.html', d=general_data(),mensaje="No se encontro el post editado")
         else:
             post = posts[0]
+            cuando = post.when
             db.delete(post.key())
             post1 = Post(title = form.title.data,
                     content = Markup(form.content.data).unescape(),
                     author = users.get_current_user(),
                     noticia = form.noticia.data,
-                    etiquetas = form.etiquetas.data.split(","))
+                    etiquetas = form.etiquetas.data.split(","),
+                    when = cuando)
             post1.put()
             flash('Entrada actualizada en la base de datos.')
             #ToDo: hacer redireccion al post editado
